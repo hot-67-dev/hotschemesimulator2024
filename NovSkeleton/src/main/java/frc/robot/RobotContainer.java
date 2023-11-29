b8c7a0059f5dda46188136399dfaff5373439f39
Andreas petre <andpetrederp@gmail.com>
2023-11-29 16:24:23 -0500

    Added Deadbands

--- NovSkeleton/src/main/java/frc/robot/RobotContainer.java
+++ NovSkeleton/src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,9 @@ import frc.robot.generated.TunerConstants;
 public class RobotContainer {
   final double MaxSpeed = 6; // 6 meters per second desired top speed
   final double MaxAngularRate = Math.PI; // Half a rotation per second max angular velocity
+  double leftX;
+  double leftY;
+  double rightX;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   CommandXboxController joystick = new CommandXboxController(0); // My joystick
@@ -28,17 +31,50 @@ public class RobotContainer {
   SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
   Telemetry logger = new Telemetry(MaxSpeed);
 
+  private double deadbandLeftY(){
+    if(Math.abs(joystick.getLeftY()* joystick.getLeftY()) > 0.05){
+      leftY = joystick.getLeftY();
+      return leftY;
+    }
+    else{
+      leftY = 0;
+      return leftY;
+    }
+
+  }
+  private double deadbandLeftX(){
+       if(Math.abs(joystick.getLeftX()* joystick.getLeftX()) > 0.05){
+      leftX = joystick.getLeftX() ;
+      return leftX;
+    }
+    else{
+      leftX = 0;
+      return leftX;
+    }
+  }
+
+    private double deadbandRightX(){
+       if(Math.abs(joystick.getRightX() * joystick.getRightX()) > Math.abs(0.05)){
+      rightX = joystick.getRightX();
+      return rightX;
+    }
+    else{
+      rightX = 0;
+      return rightX;
+    }
+  }
+
   private void configureBindings() {
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-        drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+        drivetrain.applyRequest(() -> drive.withVelocityX(-deadbandLeftY()* MaxSpeed) // Drive forward with
                                                                                            // negative Y (forward)
-            .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-            .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            .withVelocityY(-deadbandLeftX() * MaxSpeed) // Drive left with negative X (left)
+            .withRotationalRate(-deadbandRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
         ));
 
     joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
     joystick.b().whileTrue(drivetrain
-        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+        .applyRequest(() -> point.withModuleDirection(new Rotation2d(-deadbandLeftY(), -deadbandLeftX()))));
 
     if (Utils.isSimulation()) {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
